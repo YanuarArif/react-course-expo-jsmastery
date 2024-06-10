5411ee6d675e474ee9bac152ce859dc123b4976d
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2024-06-10 17:18:24 +0700

    setelah aplikasi pertama/dasar setelah reset-project sdk 51 expo

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text>Hallo Semuanya!</Text>
+      <Text>Nama saya Yanuar Lukman Arif</Text>
+    </View>
+  );
+}
